bbe88341e06b8445c5c7ad1e5f0a62e79a4ba4d9
FajarY <fajaryasodana.dev@gmail.com>
2022-02-18 15:03:51 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: "Fajar Yasodana's Personal",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "My website built on top of Netlify and Gatsbyjs for my blogs and portofolio.",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
